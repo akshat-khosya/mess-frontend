03e2047b1bd16ad958f29be09bd215f9b2298d6d
akshat-khosya <76739180+akshat-khosya@users.noreply.github.com>
2022-09-03 13:44:48 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,35 @@
+import { Provider } from "react-native-paper";
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { theme } from "./src/core/theme";
+import StartScreen from "./src/screens/StartScreen";
+import LoginScreen from "./src/screens/LoginScreen";
+import RegisterScreen from "./src/screens/RegisterScreen";
+import Dashboard from "./src/screens/Dashboard";
+import ResetPasswordScreen from "./src/screens/ResetPasswordScreen";
+
+const Stack = createStackNavigator()
+export default function App() {
+  return (
+    <Provider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="StartScreen"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="StartScreen" component={StartScreen} />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+          <Stack.Screen name="Dashboard" component={Dashboard} />
+          <Stack.Screen
+            name="ResetPasswordScreen"
+            component={ResetPasswordScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+}
+
